72d4fc599c45e29d93a23ca197d46d48e9b25672
coresyntax0330 <juancarlos.king0330@gmail.com>
2025-09-10 10:37:38 +0100

    -Update ToastProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { store } from "@/redux/stores";
 import { NextAbstractWalletProvider } from "@/components/agw-provider";
 import { Toaster } from "@/components/ui/sonner";
+import ToastProvider from "@/components/ToastProvider";
 
 // import global style
 import "@/styles/globals.css";
@@ -10,8 +11,10 @@ export default function App({ Component, pageProps }) {
   return (
     <NextAbstractWalletProvider>
       <Provider store={store}>
-        <Component {...pageProps} />
-        <Toaster />
+        <ToastProvider>
+          <Component {...pageProps} />
+          <Toaster />
+        </ToastProvider>
       </Provider>
     </NextAbstractWalletProvider>
   );
